3673ff882402ec4c84fdafd53b5bc2d7a079e676
6-MillionBees <BoettcherAr321@northwested.org>
2024-09-10 15:07:57 -0400

    Update and rename basic-practice.py to if-else-practice.py

--- if-else-practice.py
+++ if-else-practice.py
@@ -3,17 +3,17 @@
 # if-else practice
 
 
+# I don't have Python installed on my school computer and I am not able to install it myself because I need permission
+please = 'I would like to have Python installed on my computer for CTC.'
+reasons = 'Without Python I cannot debug/run any code using Visual Studio which would make it impossible to check my work.'
+extra_reasons = 'I am caught up with all of my work and want it for purely school-related reasons.'
 
-please = 'I would really like to have python installed on my computer for CTC.'
-reasons = 'Without python I cannot debug/run any code using visual studio which would make it impossible to do check my work.'
-extra_reasons = 'I am caught up with all of my work and I want it for purely school related reasons.'
-
-print(please)
+print(please) 
 print('do you agree?')
 
 check1 = input()
 
-if check1 == 'no':
+if check1 == 'no': # I have absolutely no clue if this works or not bc I haven't been able to bug-test it
     print(reasons)
     print('please say yes')
     check2 = input()
