0c047b061ff40749f5d708fa4ccc6fb71124e71b
6MillionBees <BoettcherAr321@northwested.org>
2024-09-19 13:53:26 -0400

    Update simple-recursive-sequence-solver.py

--- simple-recursive-sequence-solver.py
+++ simple-recursive-sequence-solver.py
@@ -5,7 +5,7 @@
 import math
 import string
 
-superscript_map = { # this whole thing is because I want the powers and square roots to be in superscript
+superscript_map = { # This whole thing is because I want the powers and square roots to be in superscript
     "0": "⁰", "1": "¹", "2": "²", "3": "³", "4": "⁴", "5": "⁵", "6": "⁶",
     "7": "⁷", "8": "⁸", "9": "⁹", "a": "ᵃ", "b": "ᵇ", "c": "ᶜ", "d": "ᵈ",
     "e": "ᵉ", "f": "ᶠ", "g": "ᵍ", "h": "ʰ", "i": "ᶦ", "j": "ʲ", "k": "ᵏ", 
@@ -23,13 +23,13 @@ trans = str.maketrans(
 
 main = input('\nput 3 numbers in a sequence THEY MUST BE SEPARATED WITH A SPACE:\n\n')
 
-listmain = main.split() # this whole thing makes the variable main a functioning tuple
+listmain = main.split() # This whole thing makes the variable main a functioning tuple
 tuplemain = tuple(listmain) # I also stole this from a guy on stack overflow
 
 tuple0 = float(tuplemain[0])
 tuple1 = float(tuplemain[1])
 tuple2 = float(tuplemain[2])
-num3c1 = tuple1 - tuple0 # this may look complicated but it's just defining variables
+num3c1 = tuple1 - tuple0 # This may look complicated but it's just defining variables
 num3c2 = tuple2 - tuple1
 num3c3 = tuple1 / tuple0
 num3c4 = tuple2 / tuple1
@@ -38,11 +38,11 @@ num2 = tuple0
 power = 0
 root = 0
 
-while num > 1: # this decides if it's to the power of anything and then counts how many powers
+while num > 1: # This decides if it's to the power of anything and then counts how many powers
     num /= tuple0
     power += 1 
 
-while num2 > 1: # this is for square roots
+while num2 > 1: # This is for square roots
     num2 /= tuple1
     root += 1 
 
