1553646e1da0d94b15cc8bfbdc8e271da146645b
6MillionBees <BoettcherAr321@northwested.org>
2024-09-27 14:39:00 -0400

    Update simple-recursive-sequence-solver.py

--- simple-recursive-sequence-solver.py
+++ simple-recursive-sequence-solver.py
@@ -5,9 +5,9 @@
 import math
 import string
 
-superscript_map = { # This whole thing is because I want the powers and square roots to be in superscript
+superscript_map = { # this whole thing is because I want the powers and square roots to be in superscript
     "0": "⁰", "1": "¹", "2": "²", "3": "³", "4": "⁴", "5": "⁵", "6": "⁶",
-    "7": "⁷", "8": "⁸", "9": "⁹"} # I literally just stole this from a guy on stack overflow
+    "7": "⁷", "8": "⁸", "9": "⁹"} # literally just stole this from a guy on stack overflow
 
 trans = str.maketrans(
     ''.join(superscript_map.keys()),
@@ -15,36 +15,49 @@ trans = str.maketrans(
 
 main = input('\nput 3 numbers in a sequence THEY MUST BE SEPARATED WITH A SPACE:\n\n')
 
-listmain = main.split() # This whole thing makes the variable main a functioning tuple
+listmain = main.split() # this whole thing makes the variable main a functioning tuple
 tuplemain = tuple(listmain) # I also stole this from a guy on stack overflow
 
 tuple0 = float(tuplemain[0])
 tuple1 = float(tuplemain[1])
 tuple2 = float(tuplemain[2])
-num3c1 = tuple1 - tuple0 # This may look complicated but it's just defining variables
-num3c2 = tuple2 - tuple1
+num3c1 = tuple1 - tuple0 # this may look complicated but it's just defining variables
+num3c2 = tuple2 - tuple1 # The reason there's two of each is for confirming that the sequence using addition or multiplication
 num3c3 = tuple1 / tuple0
 num3c4 = tuple2 / tuple1
-num = tuple1
-num2 = tuple0
+dif1 = num3c2 / num3c1
+dif2 = dif1*tuple0 - tuple1
+rounded_dif1 = float(f'{dif1:.4f}') # I got shifted geometric to be working
+rounded_dif2 = float(f'{dif2:.4f}') # I'm so fucking smart
+is_shifted_plus = rounded_dif1 * tuple0 + rounded_dif2 == tuple1
+is_shifted_minus = rounded_dif1 * tuple0 - rounded_dif2 == tuple1
+
+
+exponent_num = tuple1
+exponent_num2 = tuple0
 power = 0
 root = 0
 
-while num > 1: # This decides if it's to the power of anything and then counts how many powers
-    num /= tuple0
+
+while exponent_num > 1: # this decides if it's to the power of anything and then counts how many powers
+    exponent_num /= tuple0
     power += 1 
 
-while num2 > 1: # This is for square roots
-    num2 /= tuple1
+while exponent_num2 > 1: # this is for square roots
+    exponent_num2 /= tuple1
     root += 1 
 
 if num3c1 == num3c2: # This is where the output is decided and finally printed
     print(f'\nUn = Un-1 + ({num3c1})\n')
 elif num3c3 == num3c4:
     print(f'\nUn = Un-1 (*{num3c3})\n')
-elif num == 1:
-    print(f'\nUn = Un-1 ({str(power).translate(trans)})\n')
-elif num2 == 1:
+elif exponent_num == 1:
+    print(f'\nUn = Un-1({str(power).translate(trans)})\n')
+elif exponent_num2 == 1:
     print(f'\nUn = ({str(root).translate(trans)}√)Un-1\n')
+elif is_shifted_plus:
+    print(f'Un = ({rounded_dif1})Un(+{rounded_dif2})')
+elif is_shifted_minus:
+    print(f'Un = ({rounded_dif1})Un(-{rounded_dif2})')
 else:
     print('\nyou did not math correctly\n')
